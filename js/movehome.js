9f6fbc5095beb45bc3a1edcdf278fc19257d33aa
standstillsh <44561145+standstillsh@users.noreply.github.com>
2020-11-26 19:56:55 +0900

    d

---
+++ js/movehome.js
@@ -0,0 +1,10 @@
+// menu.js
+console.log("Script Load");
+(function($) {
+    $(document).ready(function() {
+        console.log('jQuery Ready');
+
+            location.href = './home.html'
+
+    });
+})(jQuery);
